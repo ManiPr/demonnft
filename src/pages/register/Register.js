ab29a000c7f16695b0b52b1763635629136b8458
ManiGeek <maniweeb@gmail.com>
2024-05-18 17:45:24 +0330

    feat:folderStructor

---
+++ src/pages/register/Register.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+import "./Register.css";
+
+export default function Index() {
+
+  return (
+    <>
+    <p>Register</p>
+    </>
+  );
+}
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import Index from "./pages/index/Index"
+import Register from './pages/register/Register'
+
+const routes = [
+    { path: '/', element: <Index /> },
+    { path: '/register', element: <Register /> },
+]
+
+export default routes
